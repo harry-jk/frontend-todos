3dfecfbe8aeff0a14c64e9a340cec2054a8a1371
Jinhyeok Kang <jinhyeok.kang@gmail.com>
2015-03-12 17:40:08 +0900

    add todo text field event

--- todos.js
+++ todos.js
@@ -1,1 +1,28 @@
 //start...
+
+/** Function Define.. **/
+
+/**
+ * Find HTML Dom Object by id
+ *
+ * @param id {String}
+ * @returns {HTMLElement}
+ */
+function getDom(id) {
+    return document.getElementById(id);
+}
+
+
+/** Init **/
+var todoStringField = getDom("todo_string");
+var todoForm = getDom("todo_form");
+
+todoForm.addEventListener('keyup', function(ev) {
+    //console.log(ev.keyCode);
+    //console.log(todoStringField.value);
+    if(ev.keyCode === 13) {
+        console.log(todoStringField.value);
+        var newTodo = todoStringField.value;
+        todoStringField.value = "";
+    }
+});
